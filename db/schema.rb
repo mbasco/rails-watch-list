fa33023258fb2623fd02fce7e25f04dc1fcdea7d
Michelle Basco <hello@mbasco.com>
2021-07-16 01:40:02 +0900

    create views and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_095820) do
+ActiveRecord::Schema.define(version: 2021_07_15_155239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 2021_07_14_095820) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
-    t.integer "rating"
+    t.float "rating"
     t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_reviews_on_list_id"
   end
 
